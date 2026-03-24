53c12147d9f134e4e0812906f803c4579d9fbc24
saracsilva <saracarolinasilva@gmail.com>
2026-03-24 17:46:24 +0100

    add session context and connect login to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,54 @@
+import { useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { login } from '../api/auth';
+import { SessionContext } from '../context/SessionContext';
+
 function Login() {
-  return <h1>Login</h1>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { saveToken } = useContext(SessionContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await login({ email, password });
+      saveToken(response.data.token);
+      navigate('/dashboard');
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>LogIn</h1>
+      <form onSubmit={handleSubmit}>
+        <div className='form__field'>
+          <label htmlFor='email'>E-mail</label>
+          <input
+            type='email'
+            id='email'
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='form__field'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            placeholder='Enter your password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
